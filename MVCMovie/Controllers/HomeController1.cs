14d6482ef476a1b41ce6b535132f680115a3097a
Hana Parsa <hanaparsa8080@gmail.com>
2024-02-22 08:23:35 +0330

    mvc

---
+++ MVCMovie/Controllers/HomeController1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCMovie.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Welcom(string message, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello" + message;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+    }
+}
---
+++ MVCMovie/Data/MVCMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCMovie.Models;
+
+namespace MVCMovie.Data
+{
+    public class MVCMovieContext : DbContext
+    {
+        public MVCMovieContext (DbContextOptions<MVCMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
